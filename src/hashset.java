c2d54746a9b4afd9322b63707f4b9a82363e3b86
vanshchoudaryy <vansh.choudhary@bounteous.com>
2026-02-19 16:52:41 +0530

    hashset

---
+++ src/hashset.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class hashset{
+    public static void main(String[] args){
+        HashSet<String> set = new HashSet<>();
+        set.add("vansh");
+        set.add("is good");
+        set.add("boy");
+
+        System.out.println("added the elements" + set);
+    }
+}
