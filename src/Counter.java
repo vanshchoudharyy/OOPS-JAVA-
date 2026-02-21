b9a21e040746df03472ab6a0309230c7d53a040b
vanshchoudaryy <vansh.choudhary@bounteous.com>
2026-02-22 00:21:42 +0530

    couneter

--- src/Counter.java
+++ src/Counter.java
@@ -1,10 +1,7 @@
-public class Counter{
-    private int count = 0;
+class Counter {
+    int count = 0;
 
-    public synchronized void increment(){
+    public synchronized void increment() {
         count++;
-    }
-    public int getCount(){
-        return count;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Counter c = new Counter();
 
-        Thread t1obj = new Thread(() -> {
-            System.out.println("hello from thread 1");
+        Thread t1 = new Thread(() -> {
+            for(int i = 0; i< 1000; i++)
+                c.increment();
         });
-        Thread t2obj = new Thread(() -> {
-            System.out.println("hello from thread 2 ");
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++)
+                c.increment();
         });
-        Thread t1 = new Thread(t1obj);
-        Thread t2 = new Thread(t2obj);
+
         t1.start();
         t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println(c.count);
     }
+
 }
